82187715207273bad4a2f66725f608a9bd015d0b
Minh Tran <minhtq9700@gmail.com>
2022-02-05 10:29:58 +0700

    ok

--- Train/js.js
+++ Train/js.js
@@ -1,63 +1,19 @@
-let posts = [
+const body = document.querySelector('body')
+const range = document.querySelector('.range')
+const rangeBar = document.querySelector('.range-bar')
 
-    {
-        name: "Tran Quang Minh",
-        class: "KTN59CL",
-        code: "79048"
-    },
-
-    {
-        name: "Tran Quang Minh",
-        class: "KTN59CL",
-        code: "79048"
-    },
-
-
-    {
-        name: "Tran Quang Minh",
-        class: "KTN59CL",
-        code: "79048"
-    },
-
-
-    {
-        name: "Tran Quang Minh",
-        class: "KTN59CL",
-        code: "79048"
-    },
-
-
-];
-
-const ip = document.querySelector("input");
-
-const leftPost = document.querySelector(".left");
-
-//cac buoc lam:
-//b1:event input clear data 
-//render data by value 
-
-function render(array, container) {
-
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
-
-        container.innerHTML = `
-        
-        <div class="post">
-
-            <img src="./fio.jpg" alt="">
-
-            <h4>${element.name}</h4>
-            <p>${element.class}</p>
-            <small>${element.code}</small>
-
-        </div>
-
-        
-        `
-
-    }
-
-
+function setRangeBar(percent) {
+    rangeBar.style.width = `${percent}%`
+    rangeBar.querySelector('span').innerText = `${percent}%`
+    body.style.backgroundColor = `rgba(0, 0, 0, ${percent / 100})`
 }
+
+setRangeBar(40)
+
+range.addEventListener('mousemove', function(e) {
+    const process = e.pageX - this.offsetLeft
+    let percent = (process / this.offsetWidth) * 100
+
+    percent = Math.ceil(percent)
+    setRangeBar(percent)
+})
