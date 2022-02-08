e25b0b36dd17fdd220c0241171aef4b10f53ac1a
Minh Tran <minhtq9700@gmail.com>
2022-02-08 15:58:02 +0700

    ok

--- Train/js.js
+++ Train/js.js
@@ -1,6 +1,7 @@
-const body = document.querySelector('body')
-const range = document.querySelector('.range')
-const rangeBar = document.querySelector('.range-bar')
+const range = document.querySelector(".range");
+const rangeBar = document.querySelector(".range-bar");
+const body = document.querySelector("body");
+const span = document.querySelector("span");
 
 function setRangeBar(percent) {
     rangeBar.style.width = `${percent}%`
@@ -8,12 +9,13 @@ function setRangeBar(percent) {
     body.style.backgroundColor = `rgba(0, 0, 0, ${percent / 100})`
 }
 
-setRangeBar(40)
+setRangeBar(40);
 
-range.addEventListener('mousemove', function(e) {
-    const process = e.pageX - this.offsetLeft
-    let percent = (process / this.offsetWidth) * 100
+range.addEventListener("mousemove", function(e) {
 
-    percent = Math.ceil(percent)
-    setRangeBar(percent)
+    let process = e.pageX - this.offsetLeft;
+    let percent = (process / this.offsetWidth) * 100;
+    percent = Math.ceil(percent);
+
+    setRangeBar(percent);
 })
